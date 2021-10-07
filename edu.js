10c725ed658833dbb4ece4de607e612a36ef39d6
tndnguyen <92077452+tndnguyen@users.noreply.github.com>
2021-10-07 17:32:56 +1100

    Update edu.js

--- edu.js
+++ edu.js
@@ -1,6 +1,9 @@
-// 
-Remove backgrounds 100% automatically in 5 seconds with zero clicks
+//  Remove backgrounds 100% automatically in 5 seconds with zero clicks
 
-There are approximately 20 million more interesting activities than removing backgrounds by hand.
+// There are approximately 20 million more interesting activities than removing backgrounds by hand.
 
-Thanks to remove.bg's clever AI, you can slash editing time - and have more fun!
+// Thanks to remove.bg's clever AI, you can slash editing time - and have more fun!
+
+function removeBg(img) {
+ // remove background 
+}
